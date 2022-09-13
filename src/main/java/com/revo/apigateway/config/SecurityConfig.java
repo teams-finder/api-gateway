1645d6d0ebef517a3d6e0409fd708951994a43fe
Tegridy <jarjak4@gmail.com>
2022-09-13 19:35:48 +0200

    Update gateway routes

--- src/main/java/com/revo/apigateway/config/SecurityConfig.java
+++ src/main/java/com/revo/apigateway/config/SecurityConfig.java
@@ -2,20 +2,44 @@ package com.revo.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
 
 @Configuration
 class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity){
-        return httpSecurity
+
+
+
+        return httpSecurity.cors(ServerHttpSecurity.CorsSpec::disable)
+                .csrf().disable()
                 .authorizeExchange()
                 .anyExchange()
-                .authenticated()
-                .and()
-                .oauth2Login()
+                .permitAll()
+                //.authenticated()
                 .and()
                 .build();
     }
